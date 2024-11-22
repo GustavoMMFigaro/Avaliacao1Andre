183771e8d9dc4123d8e81ca2203427785ff2924b
João Gabriel <joao.neves@edu.unifil.br>
2024-11-22 20:27:50 -0300

    commit
    adicionadas 3 operações

--- cod.py
+++ cod.py
@@ -1,7 +1,30 @@
-import os
+ if menu == 1:
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
+       result = x + y
+       print("O resultado é: ",result)
+       historico.append(f"Soma: {x} + {y} = {result}")
 
-def limpar_tela():
-    if os.name == 'nt':  
-        os.system('cls')
-    else:  
-        os.system('clear')
+    elif menu == 2:
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
+       result = x - y
+       print("O resultado é: ",result)
+       historico.append(f"Subtração: {x} - {y} = {result}")
+
+    elif menu == 3:
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
+       result = x * y
+       print("O resultado é: ",result)
+       historico.append(f"Subtração: {x} * {y} = {result}")
+
+    elif menu == 4:
+       x = int(input("Qual o primeiro N: "))
+       y = int(input("Qual o segundo N: "))
+       result = x // y
+       print("O resultado é: ",result)
+       historico.append(f"Divisão: {x} / {y} = {result}")
+       if x or y == 0:
+        print("Divisão por Zero não permitida.")
+    
